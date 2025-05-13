44de3a6aec35693000dcaafbfd00af0a4d45d2c2
Blake McDonald <mcdonald.blake90@gmail.com>
2025-05-13 09:46:45 -0700

    Task A & B. Project is set up

---
+++ src/main/java/com/bmcd134_D288/Bmcd134D288Application.java
@@ -0,0 +1,13 @@
+package com.bmcd134_D288;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bmcd134D288Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bmcd134D288Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bmcd134_D288/Bmcd134D288ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmcd134_D288;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bmcd134D288ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
