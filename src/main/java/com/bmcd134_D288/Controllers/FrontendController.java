7923c45953c878ab8d33a4a04eee88dd148e1b97
mcdonaldblake <mcdonald.blake90@gmail.com>
2025-05-30 11:52:14 -0700

    Update FrontendController.java

--- src/main/java/com/bmcd134_D288/Controllers/FrontendController.java
+++ src/main/java/com/bmcd134_D288/Controllers/FrontendController.java
@@ -1,12 +1,13 @@
+package com.bmcd134_D288.Controllers;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class FrontendController {
 
-    @RequestMapping(value = { "/", "/{path:^(?!api).*$}" })
+    @RequestMapping(value = { "/", "/{path:^(?!api$).*$}", "/**/{path:^(?!api$).*$}" })
     public String forward() {
         return "forward:/index.html";
     }
 }
-
